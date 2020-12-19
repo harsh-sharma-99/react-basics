238f845c9e85c3121ca9bbbe1aa570a569eb605b
harsh-sharma-99 <sharmaharsh9944@gmail.com>
2020-12-19 12:14:00 +0530

    second commit

---
+++ src/Components/Counter.js
@@ -0,0 +1,25 @@
+import React,{useState} from 'react';
+
+function Counter(){
+    const [count,setCount]= useState(0);
+    
+    return(
+    <div>
+        <h1>
+            {count}
+        </h1>
+
+        <h1 onClick={() => setCount(count+1)}>
+            PLUS
+        </h1>
+
+        <h1 onClick={() => setCount(count-1)}>
+        MINUS
+        </h1>
+    </div>
+
+          )
+
+
+}
+export default Counter;
---
+++ src/Components/Name.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Name(prop){
+    return(
+        <h1> HELLO {prop.name}</h1>
+    )
+}
+
+export default Name;
