403ebff80aedc522e5994c286a973463a67aed39
harsh-sharma-99 <sharmaharsh9944@gmail.com>
2020-12-29 00:54:32 +0530

    Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import './App.css'
 import Name from './Components/Name'
 import Counter from './Components/Counter'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
 
 function App() {
   return (
     <div>
+      <Header />
       <Name name="JARVIS"/>
       <Counter />
+      <Footer/>
     </div>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer(){
+    return(
+        <footer className="bg-gray-200 font-bold p-3 text-center absolute bottom-0 w-full">
+            &copy; Copyright 2020
+        </footer>
+    );
+}
+
+export default Footer;
